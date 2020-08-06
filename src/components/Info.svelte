59bb2936e96747338c85773e1548f725d7b621d3
Rob R <r0brh0d3s@gmail.com>
2020-08-06 12:46:37 -0700

    info component component

---
+++ src/components/Info.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let listing;
+    const { address, imageurl, price, id, neighborhood, summary } = listing;
+</script>
+
+<style>
+     ul {
+    padding: 0;
+    list-style: none;
+  }
+  .address {
+    font-size: 1.25em;
+    color: #000;
+  }
+  li {
+    font-size: .75em;
+    color: #8b8b8b;
+  }
+</style>
+
+<ul class="info">
+    <li class="address">{address}</li>
+    <li>{neighborhood}</li>
+    <li>${price}</li>
+    <li>{summary}</li>
+  </ul>
